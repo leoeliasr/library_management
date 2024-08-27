c852d89c388378c424ccbc8eef1ead882179e59e
iara campos <iarac.rodrigues23@gmail.com>
2024-08-27 13:07:33 -0300

    Criando algumas telas

---
+++ app/db/seeds.rb
@@ -0,0 +1,7 @@
+Livro.create([
+  { nome: 'Clean Code: A Handbook of Agile Software Craftsmanship', isbn: '9780132350884', autor: 'Robert C. Martin', genero: 'Software Engineering', disponivel: true },
+  { nome: 'The Pragmatic Programmer: Your Journey to Mastery', isbn: '9780201616224', autor: 'Andrew Hunt, David Thomas', genero: 'Software Development', disponivel: true },
+  { nome: 'Design Patterns: Elements of Reusable Object-Oriented Software', isbn: '9780201633610', autor: 'Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides', genero: 'Software Architecture', disponivel: false },
+  { nome: 'Refactoring: Improving the Design of Existing Code', isbn: '9780201485677', autor: 'Martin Fowler', genero: 'Software Engineering', disponivel: true },
+  { nome: 'Introduction to Algorithms', isbn: '9780262033848', autor: 'Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein', genero: 'Algorithms', disponivel: false }
+])
