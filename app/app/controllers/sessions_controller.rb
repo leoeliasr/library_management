2c6dc2e131294b9e35ceea40a70ae9200ba5aa06
iara campos <iarac.rodrigues23@gmail.com>
2024-08-28 13:16:00 -0300

    Arrumando login

--- app/app/controllers/sessions_controller.rb
+++ app/app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user&.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to root_path, notice: 'Login bem-sucedido!'
+      redirect_to root_path
     else
       flash.now[:alert] = 'Email ou senha inválidos.'
       render :new
@@ -15,6 +15,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: 'Logout realizado com sucesso.'
+    redirect_to root_path,notice: 'Logout realizado com sucesso.'
   end
 end
